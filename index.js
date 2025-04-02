3623440946510cebc1b17502338aff6cdf0a42b2
Bernardo Girardi <156864053+BernardoGirardi@users.noreply.github.com>
2025-04-01 21:06:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {Sequelize} from "sequelize";
 
+const sequelize = new Sequelize('biblioteca2025', 'postgres', 'Mxk&6qh8', {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Conexão feita com sucesso!');
+  } catch (error) {
+    console.error('Falha na conexão:', error);
+  }
+  
 const app = express();
 app.use(express.json());
 
